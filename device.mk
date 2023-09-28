e214bdc2e93754cf7ada92d2f4537777995dbc40
menorziin <williamsilvapppl@gmail.com>
2023-09-28 14:00:56 -0300

    hanoip: Import moto audio configs
    * From: Hanoip S2RIS32.32-20-7-7
    
    Change-Id: I6a3686caa474080bb5e3759edc403639ab057d31

--- device.mk
+++ device.mk
@@ -4,6 +4,12 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/audio/audio_ext_spkr.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_ext_spkr.conf \
+    $(LOCAL_PATH)/configs/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
+    $(LOCAL_PATH)/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
+
 # Call the proprietary setup
 $(call inherit-product, vendor/motorola/hanoip/hanoip-vendor.mk)
 
